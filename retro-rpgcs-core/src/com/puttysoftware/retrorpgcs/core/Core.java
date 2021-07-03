a7040d6a25f204bfa719db19515c492ffe710652
Eric Ahnell <eric.ahnell@puttysoftware.com>
2021-07-02 20:15:05 -0400

    The Core is now also a game shell.

--- retro-rpgcs-core/src/com/puttysoftware/retrorpgcs/core/Core.java
+++ retro-rpgcs-core/src/com/puttysoftware/retrorpgcs/core/Core.java
@@ -1,28 +1,14 @@
 package com.puttysoftware.retrorpgcs.core;
 
-import com.puttysoftware.diane.Diane;
+import com.puttysoftware.gameshell.ExceptionMessageConfiguration;
+import com.puttysoftware.gameshell.GameShell;
 import com.puttysoftware.retrorpgcs.core.security.SandboxManager;
 
-public class Core {
-    private static final GameErrorHandler errorHandler = new GameErrorHandler();
-
-    public static void initializeCore() {
-        Diane.installErrorHandler(Core.errorHandler);
+public abstract class Core extends GameShell {
+    public Core(String name, ExceptionMessageConfiguration errorSettings,
+            ExceptionMessageConfiguration warningSettings) {
+        super(name, errorSettings, warningSettings);
         System.setSecurityManager(new CoreSecurityManager());
-    }
-
-    public static void exception(final Throwable t) {
-        Core.errorHandler.handle(t);
-    }
-
-    public static void silentlyLog(final Throwable t) {
-        final RuntimeException re = new RuntimeException(t);
-        Core.errorHandler.silentlyLog(re);
-    }
-
-    public static void exceptionWithMessage(final Throwable t,
-            final String message) {
-        Core.errorHandler.handleWithMessage(t, message);
     }
 
     public static String getCachesDirectory() {
